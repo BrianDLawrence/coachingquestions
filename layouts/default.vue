805496ea29cfb5e1a25b5ba94934c25607444ab8
Brian Lawrence <Brian@BrianDLawrence.com>
2024-01-04 22:08:42 +0100

    Update copyright 2024

--- layouts/default.vue
+++ layouts/default.vue
@@ -35,7 +35,7 @@
     <AConsentModal></AConsentModal>
     <footer class="footer items-center p-4 bg-neutral text-neutral-content">
       <div class="items-center grid-flow-col">
-        <p>Copyright © 2023 Spero Autem LLC - All right reserved</p>
+        <p>Copyright © 2024 Spero Autem LLC - All right reserved</p>
       </div>
       <div class="grid-flow-col gap-4 md:place-self-center md:justify-self-end">
         <NuxtLink to="/privacy">Privacy Policy</NuxtLink>
